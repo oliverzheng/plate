098cfe83254a8e87a24a8ae50c759075d411ca58
Oliver Zheng <gitcommits@oliverzheng.com>
2019-09-15 12:16:32 -0500

    Move files into editor/

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import nullthrows from 'nullthrows';
 import './index.css';
-import App from './App';
+import App from './editor/App';
 
 ReactDOM.render(<App />, nullthrows(document.getElementById('root')));
